e214068cd055ed33527d2f2696b4bbb64b798463
JiaDongming <335683442@qq.com>
2018-05-28 23:09:21 -0700

    now mouse over worked

--- 2.JSCalendar/calendat.js
+++ 2.JSCalendar/calendat.js
@@ -1,8 +1,17 @@
- var oLi=document.getElementsByTagName('li');
-    for(var i=0; i<oLi.length;i++)
+ function activeCanlendar() 
         {
-            oLi[i].onmouseover=function () {
-                oLi[i].className='';
-            }
-            this.className='active';
-        }
+            var oDiv=document.getElementById('tab');
+            var oLi=oDiv.getElementsByTagName('li');
+            var i=0
+             
+                    for(i=0; i<oLi.length;i++) 
+                    {
+                        oLi[i].index=i;
+                        oLi[i].onmouseover=function () 
+                        {
+                            oLi[i].className=''; 
+                         }
+                           console.log(i);
+                           this.className='active'; 
+                    }
+     };
